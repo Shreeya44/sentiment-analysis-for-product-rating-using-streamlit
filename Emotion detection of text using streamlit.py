e21f57099296ffb1b7478f2013b8b1b28ebe6d8d
Shreya Parkale <116992887+Shreeya44@users.noreply.github.com>
2025-01-06 14:43:47 +0530

    Add files via upload

---
+++ Emotion detection of text using streamlit.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+import re 
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+def read_data(file):
+    data = []
+    with open(file, 'r')as f:
+        for line in f:
+            line = line.strip()
+            label = ' '.join(line[1:line.find("]")].strip().split())
+            text = line[line.find("]")+1:].strip()
+            data.append([label, text])
+    return data
+
+file = 'text1.txt'
+data = read_data(file)
+print("Number of instances: {}".format(len(data)))
+
+
+# In[24]:
+
+
+def ngram(token, n): 
+    output = []
+    for i in range(n-1, len(token)): 
+        ngram = ' '.join(token[i-n+1:i+1])
+        output.append(ngram) 
+    return output
+
+def create_feature(text, nrange=(1, 1)):
+    text_features = [] 
+    text = text.lower() 
+    text_alphanum = re.sub('[^a-z0-9#]', ' ', text)
+    for n in range(nrange[0], nrange[1]+1): 
+        text_features += ngram(text_alphanum.split(), n)    
+    text_punc = re.sub('[a-z0-9]', ' ', text)
+    text_features += ngram(text_punc.split(), 1)
+    return Counter(text_features)
+
+
+# In[25]:
+
+
+def convert_label(item, name): 
+    items = list(map(float, item.split()))
+    label = ""
+    for idx in range(len(items)): 
+        if items[idx] == 1: 
+            label += name[idx] + " "
+    
+    return label.strip()
+
+emotions = ["joy", 'fear', "anger", "sadness", "disgust", "shame", "guilt"]
+
+X_all = []
+y_all = []
+for label, text in data:
+    y_all.append(convert_label(label, emotions))
+    X_all.append(create_feature(text, nrange=(1, 4)))
+
+
+# In[26]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size = 0.2, random_state = 123)
+
+def train_test(clf, X_train, X_test, y_train, y_test):
+    clf.fit(X_train, y_train)
+    train_acc = accuracy_score(y_train, clf.predict(X_train))
+    test_acc = accuracy_score(y_test, clf.predict(X_test))
+    return train_acc, test_acc
+
+from sklearn.feature_extraction import DictVectorizer
+vectorizer = DictVectorizer(sparse = True)
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
+
+
+# In[27]:
+
+
+svc = SVC()
+lsvc = LinearSVC(random_state=123)
+rforest = RandomForestClassifier(random_state=123)
+dtree = DecisionTreeClassifier()
+
+clifs = [svc, lsvc, rforest, dtree]
+
+# train and test them 
+print("| {:25} | {} | {} |".format("Classifier", "Training Accuracy", "Test Accuracy"))
+print("| {} | {} | {} |".format("-"*25, "-"*17, "-"*13))
+for clf in clifs: 
+    clf_name = clf.__class__.__name__
+    train_acc, test_acc = train_test(clf, X_train, X_test, y_train, y_test)
+    print("| {:25} | {:17.7f} | {:13.7f} |".format(clf_name, train_acc, test_acc))
+
+
+# In[28]:
+
+
+l = ["joy", 'fear', "anger", "sadness", "disgust", "shame", "guilt"]
+l.sort()
+label_freq = {}
+for label, _ in data: 
+    label_freq[label] = label_freq.get(label, 0) + 1
+
+# print the labels and their counts in sorted order 
+for l in sorted(label_freq, key=label_freq.get, reverse=True):
+    print("{:10}({})  {}".format(convert_label(l, emotions), l, label_freq[l]))
+
+
+# In[29]:
+
+
+emoji_dict = {"joy":"😂", "fear":"😱", "anger":"😠", "sadness":"😢", "disgust":"😒", "shame":"😳", "guilt":"😳"}
+t1 = "This looks so impressive"
+t2 = "I have a fear of dogs"
+t3 = "My dog died yesterday"
+t4 = "I don't love you anymore..!"
+
+texts = [t1, t2, t3, t4]
+for text in texts: 
+    features = create_feature(text, nrange=(1, 4))
+    features = vectorizer.transform(features)
+    prediction = clf.predict(features)[0]
+    print( text,emoji_dict[prediction])
+
+
+# In[30]:
+
+
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
+user_input = input("Please Rate Our Services >>: ")
+sid = SentimentIntensityAnalyzer()
+score = sid.polarity_scores(user_input)
+user_input = input("Please Rate Our Services >>: ")
+sid = SentimentIntensityAnalyzer()
+score = sid.polarity_scores(user_input)
+if score["neg"] != 0:
+      print("Negative")
+else:
+      print("Positive")
+
+
+# In[31]:
+
+
+import streamlit as st
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+st.write("# Real Time Sentiment Analysis")
+
+user_input = st.text_input("Please rate our services >>: ")
+nltk.download("vader_lexicon")
+s = SentimentIntensityAnalyzer()
+score = s.polarity_scores(user_input)
+
+if score == 0:
+    st.write(" ")
+elif score["neg"] != 0:
+    st.write("# Negative")
+elif score["pos"] != 0:
+    st.write("# Positive")
+
+
+# In[ ]:
+
+
+
+
